369f441fc35d92040b5899643eaaa3b456174906
Todd Haldeman <trh178@Todds-Air.PK5001Z>
2015-04-10 22:22:34 -0400

    plots 1 and 2 done

--- plot1.R
+++ plot1.R
@@ -1,11 +1,13 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$datetime <- as.Date(strptime(x = as.character(data$Date), format = "%d/%m/%Y"))
+begin <- as.Date("2007/02/01")
+end <- as.Date("2007/02/02")
+data.in.range <- data[data$datetime >= begin & data$datetime <= end,]
 
-
-
-with(data, hist(Global_active_power,
-                axes = FALSE,
-                col = "red", 
-                main = "Global Active Power", 
-                xlab = "Global Active Power (kilowatts)", 
-                ylab = "Frequency"))
-axis(side = 1, at = seq(0,6,2), labels = seq(0,6,2))
+png(filename = "plot1.png")
+with(data.in.range, hist(Global_active_power,
+                    col = "red", 
+                    main = "Global Active Power", 
+                    xlab = "Global Active Power (kilowatts)", 
+                    ylab = "Frequency"))
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,1 +1,21 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$datetime <- as.POSIXct(strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S"))
+begin <- as.POSIXct("2007/02/01 00:00:00")
+end <- as.POSIXct("2007/02/02 23:59:59")
+data.in.range <- data[data$datetime >= begin & data$datetime <= end,]
+
+png(filename = "plot2.png")
+with(data.in.range, plot(datetime, Global_active_power,
+                         axes = FALSE,
+                         type = "l",
+                         col = "black",
+                         xlab = "",
+                         ylab = "Global Active Power (kilowatts)"))
+
+axis(side = 1, 
+     at = c(as.POSIXct("2007/02/01 00:00:00"), 
+            as.POSIXct("2007/02/02 00:00:00"),
+            as.POSIXct("2007/02/02 23:59:59")), 
+     labels = c("Thurs", "Fri", "Sat"))
+axis(side = 2, seq(0,6,2))
+dev.off()
