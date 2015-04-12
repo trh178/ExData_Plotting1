5451ba6357d5b82f27ffdaa62a11792ab23c62a5
Todd Haldeman <trh178@gmail.com>
2015-04-11 21:32:33 -0400

    finished 3 and 4

--- plot3.R
+++ plot3.R
@@ -1,1 +1,25 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$datetime <- as.POSIXct(strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S"))
+begin <- as.POSIXct("2007/02/01 00:00:00")
+end <- as.POSIXct("2007/02/02 23:59:59")
+data.in.range <- data[data$datetime >= begin & data$datetime <= end,]
+
+png(filename = "plot3.png")
+with(data.in.range, plot(datetime, Sub_metering_1,
+                         axes = FALSE,
+                         type = "l",
+                         col = "black",
+                         xlab = "",
+                         ylab = "Energy sub metering"))
+with(data.in.range, lines(datetime, Sub_metering_2, type = "l", col = "red"))
+with(data.in.range, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1,1))
+box()
+
+axis(side = 1, 
+     at = c(as.POSIXct("2007/02/01 00:00:00"), 
+            as.POSIXct("2007/02/02 00:00:00"),
+            as.POSIXct("2007/02/02 23:59:59")), 
+     labels = c("Thurs", "Fri", "Sat"))
+axis(side = 2, seq(0,30,10))
+dev.off()
--- plot4.R
+++ plot4.R
@@ -1,10 +1,18 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$datetime <- as.POSIXct(strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S"))
+begin <- as.POSIXct("2007/02/01 00:00:00")
+end <- as.POSIXct("2007/02/02 23:59:59")
+data.in.range <- data[data$datetime >= begin & data$datetime <= end,]
 
+png(filename = "plot4.png")
 par(mfrow = c(2,2))
-with(data, {
-    plot()
-    plot()
-    plot()
-    plot()
-    mtext("some title")
+with(data.in.range, {
+    plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+    plot(datetime, Voltage, type = "l")
+    plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+    lines(datetime, Sub_metering_2, type = "l", col = "red")
+    lines(datetime, Sub_metering_3, type = "l", col = "blue")
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1,1), bty = "n")
+    plot(datetime, Global_reactive_power, type = "l")
 })
+dev.off()
